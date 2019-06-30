44ab7af22b3b18b63587693cf0a3dc5c54eebf7b
WanFengZ <1604574606@qq.com>
2019-06-30 12:31:40 +0800

    add city header

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <span class="iconfont header-back">&#xe8b5;</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/varibles.styl'
+  .header
+    overflow hidden
+    height $header-height
+    line-height $header-height
+    text-align center
+    font-size .32rem
+    color #fff
+    background-color $bgColor
+    .header-back
+      width .64rem
+      font-size .4rem
+      color #fff
+      position absolute
+      top 0
+      left .2rem
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe8d6;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe8d2;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe8d2;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,8 +29,8 @@ export default {
   @import "~@styles/varibles.styl"
   .header
     display flex
-    height .86rem
-    line-height .86rem
+    height $header-height
+    line-height $header-height
     background-color $bgColor
     color #fff
     .header-left
@@ -41,6 +43,7 @@ export default {
       width 1.24rem
       float right
       text-align center
+      color: #fff
       .arrow-icon
         font-size .24rem
         margin-left -0.06rem
