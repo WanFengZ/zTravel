15695007e4d351d7674fa06cc282d2df3521a3d6
WanFengZ <1604574606@qq.com>
2019-07-01 19:46:28 +0800

    add detail header

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <span class="header-abs-icon iconfont">&#xe8b5;</span>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      景点详情
+      <router-link to="/">
+        <span class="iconfont header-back">&#xe8b5;</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 120
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity
+        }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@styles/varibles.styl'
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .8rem
+    height .8rem
+    border-radius .4rem
+    background: rgba(0,0,0,.6)
+    color #fff
+    .header-abs-icon
+      font-size .48rem
+      line-height .8rem
+      margin-left .26rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    overflow hidden
+    height $header-height
+    line-height $header-height
+    text-align center
+    font-size .32rem
+    color #fff
+    background-color $bgColor
+    .header-back
+      width .64rem
+      font-size .4rem
+      color #fff
+      position absolute
+      top 0
+      left .2rem
+</style>
