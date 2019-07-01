4688d81ce542f3a2103f53b1acb26a5193990205
WanFengZ <1604574606@qq.com>
2019-07-01 17:25:40 +0800

    add detail banner

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container" @click.self="handleCancel">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide  v-for="(item, index) of imgs" :key="index">
+          <img class="swiper-img" :src="item" alt="">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleCancel () {
+      this.$emit('cancel')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #000
+    .wrapper
+      width 100%
+      .swiper-img
+        width  100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="banner" @click="handleEnter">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1906/93/93db3b8f65fb15b4a3.water.jpg_600x330_9fd83df8.jpg" alt="">
+      <div class="banner-title">深圳欢乐谷(AAAAA景区)</div>
+      <div class="banner-number">
+        <span class="iconfont">&#xe889;</span>
+        39
+      </div>
+    </div>
+    <common-gallery :imgs="imgs" v-show="showGallery" @cancel="handleCancel"></common-gallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from '@common/gallery/Gallery'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallery
+  },
+  data () {
+    return {
+      showGallery: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1604/e0/e074ca39741daad890.water.jpg_r_800x800_0b01554c.jpg',
+        'http://img1.qunarzz.com/sight/p0/1604/80/80941a74399235d790.water.jpg_r_800x800_24576495.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleEnter () {
+      this.showGallery = true
+    },
+    handleCancel () {
+      this.showGallery = false
+    }
+  },
+  activated () {
+    this.showGallery = false
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-title
+      position absolute
+      bottom 0
+      left 0
+      right 0
+      padding-left .2rem
+      line-height .86rem
+      font-size .36rem
+      color: #fff
+      background: linear-gradient(rgba(255,255,255,.1),rgba(0,0,0,.8))
+    .banner-number
+      position absolute
+      left .2rem
+      bottom .8rem
+      padding  .05rem .2rem
+      background-color rgba(0,0,0,.6)
+      border-radius .2rem
+      line-height .4rem
+      color #fff
+</style>
